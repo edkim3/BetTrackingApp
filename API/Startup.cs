3f7348e2f1ded6b2929816cb6cd939f3ef1834f2
edkim3 <94409377+edkim3@users.noreply.github.com>
2022-01-24 19:08:23 -0700

    Create connection string
    Made connection string. Update startup to connect to new connection string. Install microsoft.entityframeworkcore.design. Add new migration

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,19 +18,19 @@ namespace API
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options => 
             {
-                options.UseSqlite("Connection string");
+                options.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
             services.AddSwaggerGen(c =>
